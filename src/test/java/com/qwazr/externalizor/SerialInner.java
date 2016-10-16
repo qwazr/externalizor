24563466268790e48685480848a1aefe58d0dfbb
Emmanuel Keller <ekeller@open-search-server.com>
2016-10-17 00:54:36 +0200

    Improve UT

--- src/test/java/com/qwazr/externalizor/SerialInner.java
+++ src/test/java/com/qwazr/externalizor/SerialInner.java
@@ -16,6 +16,7 @@
 package com.qwazr.externalizor;
 
 import org.apache.commons.lang3.RandomStringUtils;
+import org.apache.commons.lang3.RandomUtils;
 
 import java.io.Externalizable;
 import java.io.IOException;
@@ -25,12 +26,60 @@ import java.util.Objects;
 
 public class SerialInner implements Externalizable {
 
-	private final String key;
-	private final String value;
+	private final String stringValue;
+	private final String stringNullValue;
+	private final int intValue;
+	private final Integer intLangValue;
+	private final Integer intNullValue;
+	private final short shortValue;
+	private final Short shortLangValue;
+	private final Short shortNullValue;
+	private final long longValue;
+	private final Long longLangValue;
+	private final Long longNullValue;
+	private final float floatValue;
+	private final Float floatLangValue;
+	private final Float floatNullValue;
+	private final double doubleValue;
+	private final Double doubleLangValue;
+	private final Double doubleNullValue;
+	private final boolean booleanValue;
+	private final Boolean booleanLangValue;
+	private final Boolean booleanNullValue;
+	private final byte byteValue;
+	private final Byte byteLangValue;
+	private final Byte byteNullValue;
+	private final char charValue;
+	private final Character charLangValue;
+	private final Character charNullValue;
 
 	public SerialInner() {
-		key = RandomStringUtils.randomAscii(8);
-		value = RandomStringUtils.randomAscii(8);
+		stringValue = RandomStringUtils.randomAscii(8);
+		stringNullValue = null;
+		intValue = RandomUtils.nextInt(0, Integer.MAX_VALUE);
+		intLangValue = RandomUtils.nextInt(0, Integer.MAX_VALUE);
+		intNullValue = null;
+		shortValue = (short) RandomUtils.nextInt(0, Short.MAX_VALUE);
+		shortLangValue = (short) RandomUtils.nextInt(0, Short.MAX_VALUE);
+		shortNullValue = null;
+		longValue = RandomUtils.nextLong(0, Long.MAX_VALUE);
+		longLangValue = RandomUtils.nextLong(0, Long.MAX_VALUE);
+		longNullValue = null;
+		floatValue = (float) RandomUtils.nextDouble(0, Float.MAX_VALUE);
+		floatLangValue = (float) RandomUtils.nextDouble(0, Float.MAX_VALUE);
+		floatNullValue = null;
+		doubleValue = RandomUtils.nextDouble(0, Double.MAX_VALUE);
+		doubleLangValue = RandomUtils.nextDouble(0, Double.MAX_VALUE);
+		doubleNullValue = null;
+		booleanValue = RandomUtils.nextInt(0, 1) == 0;
+		booleanLangValue = RandomUtils.nextInt(0, 1) == 0;
+		booleanNullValue = null;
+		byteValue = (byte) RandomUtils.nextInt(0, Byte.MAX_VALUE);
+		byteLangValue = (byte) RandomUtils.nextInt(0, Byte.MAX_VALUE);
+		byteNullValue = null;
+		charValue = (char) RandomUtils.nextInt(0, Character.MAX_VALUE);
+		charLangValue = (char) RandomUtils.nextInt(0, Character.MAX_VALUE);
+		charNullValue = null;
 	}
 
 	@Override
@@ -38,7 +87,55 @@ public class SerialInner implements Externalizable {
 		if (o == null || !(o instanceof SerialInner))
 			return false;
 		final SerialInner s = (SerialInner) o;
-		return Objects.equals(key, s.key) && Objects.equals(value, s.value);
+		if (intValue != s.intValue)
+			return false;
+		if (shortValue != s.shortValue)
+			return false;
+		if (longValue != s.longValue)
+			return false;
+		if (floatValue != s.floatValue)
+			return false;
+		if (doubleValue != s.doubleValue)
+			return false;
+		if (booleanValue != s.booleanValue)
+			return false;
+		if (byteValue != s.byteValue)
+			return false;
+		if (charValue != s.charValue)
+			return false;
+		if (!Objects.equals(stringValue, s.stringValue))
+			return false;
+		if (!Objects.equals(stringNullValue, s.stringNullValue))
+			return false;
+		if (!Objects.equals(shortLangValue, s.shortLangValue))
+			return false;
+		if (!Objects.equals(shortNullValue, s.shortNullValue))
+			return false;
+		if (!Objects.equals(longNullValue, s.longNullValue))
+			return false;
+		if (!Objects.equals(longLangValue, s.longLangValue))
+			return false;
+		if (!Objects.equals(floatNullValue, s.floatNullValue))
+			return false;
+		if (!Objects.equals(floatLangValue, s.floatLangValue))
+			return false;
+		if (!Objects.equals(doubleNullValue, s.doubleNullValue))
+			return false;
+		if (!Objects.equals(doubleLangValue, s.doubleLangValue))
+			return false;
+		if (!Objects.equals(booleanNullValue, s.booleanNullValue))
+			return false;
+		if (!Objects.equals(booleanLangValue, s.booleanLangValue))
+			return false;
+		if (!Objects.equals(byteNullValue, s.byteNullValue))
+			return false;
+		if (!Objects.equals(byteLangValue, s.byteLangValue))
+			return false;
+		if (!Objects.equals(charNullValue, s.charNullValue))
+			return false;
+		if (!Objects.equals(charLangValue, s.charLangValue))
+			return false;
+		return true;
 	}
 
 	// The serialization part
